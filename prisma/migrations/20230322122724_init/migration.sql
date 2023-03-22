ca2ac0d1e6be17b2177c29a8877e1a7c5e79aa8b
Aldo R. Robles <90198035+roblesdotdev@users.noreply.github.com>
2023-03-22 11:29:15 -0300

    initial datamodel (#1)

---
+++ prisma/migrations/20230322122724_init/migration.sql
@@ -0,0 +1,117 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Admin" (
+    "userId" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Admin_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Password" (
+    "userId" TEXT NOT NULL,
+    "hash" TEXT NOT NULL,
+    CONSTRAINT "Password_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Org" (
+    "slug" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    CONSTRAINT "Org_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Job" (
+    "slug" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "body" TEXT NOT NULL,
+    "open" BOOLEAN NOT NULL DEFAULT true,
+    "bannerUrl" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "locationSlug" TEXT NOT NULL,
+    "teamSlug" TEXT NOT NULL,
+    "orgSlug" TEXT NOT NULL,
+    CONSTRAINT "Job_locationSlug_fkey" FOREIGN KEY ("locationSlug") REFERENCES "Location" ("slug") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "Job_teamSlug_fkey" FOREIGN KEY ("teamSlug") REFERENCES "Team" ("slug") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "Job_orgSlug_fkey" FOREIGN KEY ("orgSlug") REFERENCES "Org" ("slug") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Location" (
+    "slug" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "country" TEXT NOT NULL,
+    "remote" BOOLEAN NOT NULL DEFAULT false
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Team" (
+    "slug" TEXT NOT NULL,
+    "categoryId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    CONSTRAINT "Team_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "ContactInfo" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "phone" TEXT,
+    "address" TEXT,
+    "city" TEXT,
+    "state" TEXT,
+    "zip" TEXT,
+    "email" TEXT,
+    "country" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "orgSlug" TEXT NOT NULL,
+    CONSTRAINT "ContactInfo_orgSlug_fkey" FOREIGN KEY ("orgSlug") REFERENCES "Org" ("slug") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Admin_userId_key" ON "Admin"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Password_userId_key" ON "Password"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Org_slug_key" ON "Org"("slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Job_slug_key" ON "Job"("slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Location_slug_key" ON "Location"("slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Team_slug_key" ON "Team"("slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ContactInfo_id_key" ON "ContactInfo"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ContactInfo_orgSlug_key" ON "ContactInfo"("orgSlug");
--- prisma/seed/index.ts
+++ prisma/seed/index.ts
@@ -1,5 +1,14 @@
 import { PrismaClient } from '@prisma/client'
-import { createPassword, createUser } from './utils'
+import {
+  createCategory,
+  createContactInfo,
+  createLocation,
+  createOrg,
+  createPassword,
+  createTeam,
+  createJob,
+  createUser,
+} from './utils'
 
 const prisma = new PrismaClient()
 
@@ -9,11 +18,15 @@ async function seed() {
 
   console.time('🧹 Cleaned up the database...')
   await prisma.user.deleteMany({ where: {} })
+  await prisma.category.deleteMany({ where: {} })
+  await prisma.team.deleteMany({ where: {} })
+  await prisma.location.deleteMany({ where: {} })
+  await prisma.job.deleteMany({ where: {} })
   console.timeEnd('🧹 Cleaned up the database...')
 
-  console.time('👤 Create test user...')
+  console.time('👤 Create remixer user...')
   const userData = createUser('remixer')
-  await prisma.user.create({
+  const remixer = await prisma.user.create({
     data: {
       ...userData,
       password: {
@@ -21,7 +34,108 @@ async function seed() {
       },
     },
   })
-  console.timeEnd('👤 Create test user...')
+  await prisma.admin.create({ data: { userId: remixer.id } })
+  console.timeEnd('👤 Create remixer user...')
+
+  const numUsers = 10
+  console.time(`👤 Create ${numUsers} test users...`)
+  const users = await Promise.all(
+    Array.from({ length: numUsers }, async () => {
+      const userData = createUser()
+      return await prisma.user.create({
+        data: {
+          ...userData,
+          password: {
+            create: createPassword(userData.username),
+          },
+        },
+      })
+    }),
+  )
+  console.timeEnd(`👤 Create ${numUsers} test users...`)
+
+  const numCategories = 6
+  console.time(`📋 Create ${numCategories} test categories...`)
+  const categories = await Promise.all(
+    Array.from({ length: numCategories }, async () => {
+      const categoryData = createCategory()
+      return await prisma.category.create({
+        data: { ...categoryData },
+      })
+    }),
+  )
+  console.timeEnd(`📋 Create ${numCategories} test categories...`)
+
+  const teamsPerCategory = 4
+  const numTeams = teamsPerCategory * numCategories
+  console.time(`📋 Create ${numTeams} test teams...`)
+  const catIds = categories.map(c => c.id)
+  const teams = await Promise.all(
+    Array.from({ length: teamsPerCategory }, () => {
+      return catIds.map(async id => {
+        const teamData = createTeam()
+        return await prisma.team.create({
+          data: {
+            ...teamData,
+            categoryId: id,
+          },
+        })
+      })
+    }).flat(),
+  )
+  console.timeEnd(`📋 Create ${numTeams} test teams...`)
+
+  const userIds = users.map(u => u.id)
+  console.time(`📋 Create ${userIds.length} test organizations...`)
+  const orgs = await Promise.all(
+    userIds.map(async userId => {
+      const orgData = createOrg()
+      return await prisma.org.create({
+        data: {
+          ...orgData,
+          userId,
+          contactInfo: {
+            create: createContactInfo(),
+          },
+        },
+      })
+    }),
+  )
+  console.timeEnd(`📋 Create ${numUsers} test organizations...`)
+
+  const numLocations = 25
+  console.time(`📋 Create ${numLocations} test locations...`)
+  const locations = await Promise.all(
+    Array.from({ length: numLocations }, async () => {
+      return await prisma.location.create({
+        data: createLocation(),
+      })
+    }),
+  )
+  console.timeEnd(`📋 Create ${numLocations} test locations...`)
+
+  const jobsPerOrg = 8
+  const orgIds = orgs.map(o => o.slug)
+  const locationIds = locations.map(l => l.slug)
+  const teamsIds = teams.map(t => t.slug)
+  console.time(`📋 Create ${jobsPerOrg * orgs.length} jobs...`)
+  await Promise.all(
+    orgIds.map(async slug => {
+      Array.from({ length: jobsPerOrg }, async () => {
+        return await prisma.job.create({
+          data: {
+            ...createJob(),
+            orgSlug: slug,
+            locationSlug:
+              locationIds[Math.floor(Math.random() * locationIds.length)]!,
+            teamSlug: teamsIds[Math.floor(Math.random() * teamsIds.length)]!,
+          },
+        })
+      })
+    }),
+  )
+
+  console.timeEnd(`📋 Create ${jobsPerOrg * orgs.length} jobs...`)
 
   console.timeEnd(`🌱 Database has been seeded`)
 }
